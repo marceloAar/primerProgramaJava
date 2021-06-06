cc3579fcbeb61c21b8fafa9b6da2ceec1b1e142b
MarceloAar <marcelo_aar@hotmail.com>
2021-06-06 16:39:52 -0400

    Envio Actividad de Primer Programa JAVA

---
+++ PrimerProgramaJava/javaFun/src/javaFun/Primero.java
@@ -0,0 +1,19 @@
+//Marcelo Aceituno R
+//Full Stack JAVA 0034
+package javaFun;
+
+public class Primero {
+
+	public static void main(String[] args) {
+		
+		String nombre = "El Chavo del 8";
+		int edad = 8;
+		String ciudad = "La vecindad del Chavo";
+		
+		System.out.println("- Mi nombre es: "+nombre);
+		System.out.println("- Tengo "+edad+" años de edad");
+		System.out.println("- Mi ciudad es: "+ciudad);
+		
+	}
+
+}
